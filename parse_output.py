a7dc3078cd5372a9abc8a26ac550b3bb5544d6b7
Lenneke Jong <Lenneke.Jong@synchrotron.org.au>
2013-11-28 15:49:45 +1100

    updates mostly to the output parsing

--- parse_output.py
+++ parse_output.py
@@ -16,13 +16,19 @@ class OutputParser(object):
     Parses the output from the shelx programs from 
     '''
     shelxc_stats_prefixes=re.compile(" (?P<prefix>Resl\.|N\(data\)|\<I\/sig\>|\%Complete|\<d\/sig\>).+")
-    
+     #shelxd output lines of this format
+     #Try     28, CPU 1, CC All/Weak 49.4 / 31.2, CFOM 80.7, best 80.7, PATFOM  34.1
+    shelxd_stats_prefixes=re.compile("(?P<key>[a-zA-Z\/]*)\s+(?P<val>\d+\.?\d*(?:\s\/\s\d+\.?\d*)?)")
+    shelxe_stats_prefixes=re.compile("(?P<key>[a-zA-Z\<\>]*)\.?\s+=?\s*(?P<val>\d+$|\d+\.\d+)")
+        
     def __init__(self):
         '''
         Constructor
         '''
         self.shelxc_stats={}
-
+        self.shelxd_stats={}
+        self.shelxe_stats={"Contrast":[],"Cycle":[]}
+        self.shelxei_stats={"Contrast":[],"Cycle":[]}
         
     def setOutfile(self,logfilename):
         self.logfile=open(logfilename,'w') 
@@ -33,19 +39,75 @@ class OutputParser(object):
         self.outputstring=pipe
         self.logfile.write(pipe)
         
+    def clean(self):
+        """clean up all that stuff
+        """
+        self.outputstring=None
+        
+
     def parse_shelxc_out(self):
         #search through the output string for the stats table, creates a dict with the lines and all the values
         for line in self.outputstring.split('\n'):
             m=re.match(self.shelxc_stats_prefixes,line)
             if m:
-                self.shelxc_stats[m.group('prefix')]=line.split()[1:]
+                results=[float(r) for r in line.split()[1:] if r!='Inf.']
+                self.shelxc_stats[m.group('prefix')]=results
             else:
                 pass
-        print self.shelxc_stats
         #what else should we do with them... 
+        return self.shelxc_stats
         
     def parse_shelxd_out(self):
-        pass
+        #search through the output string for the stats table, creates a dict with the lines and all the values
+        for line in self.outputstring.split('\n'):
+            m=re.match('^ Try.+',line)
+            if m:
+                m=re.findall(self.shelxd_stats_prefixes,line)
+                if m:
+                    results=dict((k,v) for k,v in m) 
+                     # now fix the all/weak key and split it
+                    ccallweak=results['All/Weak'].split('/')
+                    ccall=ccallweak[0]
+                    ccweak=ccallweak[1] #special case
+                    results['CCall']=ccall
+                    results['CCweak']=ccweak
+                    del results['All/Weak']
+                    for key,val in results.iteritems():
+                        if not key in self.shelxd_stats:
+                            self.shelxd_stats[key]=[float(val)]
+                        else:
+                            self.shelxd_stats[key].append(float(val))
+                else:
+                    pass
+            else:
+
+                pass
+        #what else should we do with them... 
+        return self.shelxd_stats
     
-    def parse_shelxe_out(self):
-        pass
+    def parse_shelxe_out(self,inverted):
+        #search through the output string for the stats table, creates a dict with the lines and all the values
+        for line in self.outputstring.split('\n'):
+            m=re.findall(self.shelxe_stats_prefixes,line)
+            if m and len(m)==4:
+                results=dict((k,v) for k,v in m)
+                for key,val in results.iteritems():
+                    if inverted:
+                        for key,val in results.iteritems():
+                            if not key in self.shelxei_stats:
+                                self.shelxei_stats[key]=[float(val)]
+                            else:
+                                self.shelxei_stats[key].append(float(val)) 
+                    else:
+                        for key,val in results.iteritems():
+                            if not key in self.shelxe_stats:
+                                self.shelxe_stats[key]=[float(val)]
+                            else:
+                                self.shelxe_stats[key].append(float(val)) 
+            else:
+                pass
+        #what else should we do with them... 
+        if not inverted:
+            return self.shelxe_stats
+        else:
+            return self.shelxei_stats
--- shelx_pipeline.py
+++ shelx_pipeline.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 from subprocess import call, Popen, PIPE, STDOUT
+#from beamline import variables as mxvariables
 from string import Template
 import sys,traceback
 import logging
@@ -7,10 +8,13 @@ import tempfile
 import argparse
 from sets import Set
 from parse_output import OutputParser
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
-class Pipeline(object):
-    # these defaults are from the example Nathan gave me
-    defaults={'FIND':9,'DSUL':7,'MIND':-3.5,'NTRY':100} 
+class AutoShelx(object):
+    # these defaults
+    defaults={'NTRY':1000} 
     possible_params=Set({'FIND','DSUL','MIND','NTRY','SPAG','SAD','CELL'})
 
     def __init__(self):
@@ -18,6 +22,11 @@ class Pipeline(object):
         self.input_params=self.defaults
         self.name=''
         self.outfile=''
+        self.results={}
+
+    def update_defaults(self,default_dict):
+        self.defaults.update(default_dict)
+
 
     def process_args(self,cell,spag,sad,**kwargs):
         self.outfile=kwargs.get('outfile',"shelx_pipeline.out")
@@ -50,9 +59,7 @@ class Pipeline(object):
                 # print sout
             # at the moment we're just printing out the output, but we want to capture it and parse it
             self.parser.read(sout)
-            self.parser.parse_shelxc_out()
-           # p2=Popen(["tee",self.outfile],stdin=PIPE)
-           # p2.communicate(sout) 
+            self.results['shelxc'] = self.parser.parse_shelxc_out()
         except:
             print "an error occured"
             traceback.print_exc()
@@ -60,24 +67,34 @@ class Pipeline(object):
             # an exception
    
     def call_shelxd(self,*args,**kwargs):
+        print "calling shelxd"
         infilename=self.name+'_fa'
-        p=Popen(["shelxd",infilename],stdout=PIPE)
-        call(["tee","-a",self.outfile],stdin=p.stdout)
-    
+        p=Popen(["shelxd",infilename],stdout=PIPE,stderr=PIPE)
+        (sout,serr)=p.communicate()
+        #call(["tee","-a",self.outfile],stdin=p.stdout)
+        self.parser.read(sout)
+        self.results['shelxd']=self.parser.parse_shelxd_out()
 
     def call_shelxe(self,*args,**kwargs):
         infilename=self.name+'_fa'
         shelxe_flags=kwargs.get('shelxe_flags',[])
         shelxe_args=['shelxe',self.name,infilename]
-        
+        inverted=False
         for a in shelxe_flags+list(args):
             shelxe_args.append(a)
         if not "-i" in shelxe_args and kwargs.get('invert',False)==True:
             shelxe_args.append("-i")
+            inverted=True
+        print "starting shelxe inverted="+str(inverted)
         p=Popen(shelxe_args,stdout=PIPE)
+        (sout,serr)=p.communicate()
         # possible arguments are -h -s0.5 =m20 -i
-        call(["tee","-a",self.outfile],stdin=p.stdout)
-    
+       # call(["tee","-a",self.outfile],stdin=p.stdout)
+        self.parser.read(sout)
+        if inverted:
+            self.results['shelxe_inverted']= self.parser.parse_shelxe_out(inverted)
+        else:
+            self.results['shelxe']= self.parser.parse_shelxe_out(inverted)
 
     def run(self,name,cell,spag,sad,*args,**kwargs):
         """ Main entry point to the pipeline. Four required positional arguments are name, cell, spag, sad
@@ -97,25 +114,63 @@ class Pipeline(object):
             print "something bad happened"
             print  traceback.print_exc(file=sys.stdout)
 
+def test_1():
+    """Test to compare with known dataset"""
+    logging.basicConfig(filename='shelx_pipeline_test1.log',level=logging.INFO)
+    logging.info('Starting the shelxc/d/e pipeline')
+    pipeline=AutoShelx()
+    cell="42.215 42.719 70.371 90.0 90.0 90.0"
+    spag="P212121"
+    name="known_data_test"
+    sad="aimless_hsymm.sca"
+    logging.info('Starting for run '+name)
+    shelxe_flags=["-h","-s0.27", "-m20"]
+    # you can call it like this with the 4 compulsory positional args and the extra kwargs
+    pipeline.run(name,cell,spag,sad,FIND=4,shelxe_flags=shelxe_flags,outfile='pipeline_test.out')
+    
+    fig,axes=plt.subplots(nrows=3,ncols=1)
+    fig.tight_layout()
+    plt.subplot(311)
+    xs=np.array(pipeline.results['shelxc']['Resl.'])
+    ys=np.array(pipeline.results['shelxc']['<I/sig>'])
+    plt.plot(xs,ys,'bo')
+    plt.axis([np.amax(xs)+1,np.amin(xs)-1,np.amin(ys)-1,np.amax(ys)+1])
+    plt.xlabel(r'Resolution [$\AA$]')
+    plt.ylabel('<I/sig>')
+    plt.title('Resolution vs <I/sig>')
+    
+    plt.subplot(312)
+    plt.plot(pipeline.results['shelxd']['CCweak'],pipeline.results['shelxd']['CCall'],'bo')
+    plt.xlabel('CCweak')
+    plt.ylabel('CCall')
+    plt.title('CCall vs CCweak')
+
+    plt.subplot(313)
+    plt.plot(pipeline.results['shelxe']['cycle'],pipeline.results['shelxe']['Contrast'],'ro',
+                  pipeline.results['shelxe_inverted']['cycle'],pipeline.results['shelxe_inverted']['Contrast'],'bo')
+    plt.xlabel('Cycle')
+    plt.ylabel('Contrast')
+    plt.title('Constrast vs Cycle')
+
+    plt.savefig('test_1_plots.png')
+    #plt.show()
+
 def main():
-    """Used for testing, the run method in the pipeline class is the relevant method to call from other python"""
     logging.basicConfig(filename='shelx_pipeline.log',level=logging.INFO)
     logging.info('Starting the shelxc/d/e pipeline')
     parser=argparse.ArgumentParser()
-    parser.add_argument("name",help="Name for this experiment?run?thing?")
+    parser.add_argument("name",help="Name for this run")
     parser.add_argument("CELL",help="A string of 6 numbers of the unit cell")
     parser.add_argument("SPAG",help="something")
     parser.add_argument("SAD", help="Name of the hkl file to input")
     argsdict=vars(parser.parse_args())
     argsdict['outfile']="pipeline_output"
-    pipeline=Pipeline()
+    pipeline=AutoShelx()
     logging.info('Starting for run '+argsdict['name'])
     shelxe_flags=["-h","-s0.5", "-m20"]
     # you can call it like this with the 4 compulsory positional args and the extra kwargs
     pipeline.run(argsdict['name'],argsdict['CELL'],argsdict['SPAG'],argsdict['SAD'],FIND=9,DSUL=8,shelxe_flags=shelxe_flags)
-
-
-    # call shelxc with t
+    
 
 if __name__ == "__main__":
-    main()
+    test_1()
